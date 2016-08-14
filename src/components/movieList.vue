e28ccf43f235a0740e83370080a938ca5c8b255f
MarkGu <jiandong.gu@carestream.com>
2016-08-14 21:16:03 +0800

    [movieList] refactory the movieList module. 1. movieList only list movie info 2. move the data handle logic outside movApp.

--- src/components/movieList.vue
+++ src/components/movieList.vue
@@ -1,21 +1,5 @@
 <template>
   <div class="container-fluid">
-    <div class="row">
-      <div class="col-xs-10">
-        <v-pagination :pagination="pagination" :offset="3"></v-pagination>
-      </div>
-      <div class="col-xs-2">
-        <select v-on:change="loadData" v-model="pagination.per_page" class="form-control right pagenum-list" debounce="300">
-          <option>5</option>
-          <option>10</option>
-          <option>20</option>
-          <option>50</option>
-          <option>100</option>
-        </select>
-        
-      </div>                 
-    </div>
-    <div class="row">
       <table class="table">
         <thead>
           <!--<tr>
@@ -40,18 +24,19 @@
           </tr>
         </tbody>
       </table>
-    </div>
-    <div class="row">
-      <div class="col-md-12"><v-pagination :pagination="pagination" :offset="3"></v-pagination></div>
-    </div>
   </div>
 </template>
 <script>
 import mSimple from './movieSimple'
 import mPic from './moviePic'
-import vPagination from 'vue-bootstrap-pagination'
 
 export default {
+  props: {
+    movies: {
+      type: Array,
+      required: true
+    }
+  },
   destroyed () {
     console.log('module movieList: destroyed')
   },
@@ -60,81 +45,29 @@ export default {
   },
   created () {
     console.log('module movieList: created')
-    if (this.movies && this.movies.length === 0) {
-      this.loadData()
-    }
   },
   init () {
     console.log('module movieList: init')
   },
   watch: {
-    // 'pagination.current_page': function () {
-    //   this.loadData()
-    // }
-  },
-  events: {
-    evtPageChanged: function (page) {
-      // if (this.pagination.current_page !== page) {
-      //   this.pagination.current_page = page
-      //   this.loadData()
-      // }
-      // this.pagination.current_page = page
-      this.loadData()
-    }
+
   },
   data () {
     return {
-      movies: [],
-      // resource_url: '/papi/movies'
-      pagination: {
-        last_page: 0,
-        per_page: 10,
-        from: 1,
-        to: 1,
-        current_page: 1
-      }
     }
   },
   computed: {
 
   },
-  methods: {
-    // add methods here
-    loadData: function () {
-      this.$http.get('/papi/movies/' + this.pagination.current_page + '/' + this.pagination.per_page).then(function (response) {
-        var rdata = JSON.parse(response.data)
-        this.$set('movies', rdata.movies)
-        this.$set('pagination', rdata.pagination)
-        // save it to the app cache
-        this.$dispatch('evtGetMoviesData', rdata.movies)
-      }, function (error) {
-        // handle error
-        console.log('get movies data failed!!!' + error)
-      })
-    }
-  },
   components: {
     // add components here
     mSimple,
-    mPic,
-    vPagination
+    mPic
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.right {
-    margin: 20px 0;
-    float: right;
-    /*width: 300px;
-    border: 3px solid #73AD21;
-    padding: 10px;
-    vertical-align: middle;*/
-    /*align-items:center;
-    align-content:center;*/
-}
-.pagenum-list {
-  width: 75px;
-}
+
 </style>
--- src/movApp.vue
+++ src/movApp.vue
@@ -1,20 +1,91 @@
 <template>
   <div id="mov"  class="center-block">
-    <m-list></m-list>
+    <div class="row">
+      <div class="col-xs-10">
+        <v-pagination :pagination="pagination" :offset="3"></v-pagination>
+      </div>
+      <div class="col-xs-2">
+        <select v-on:change="loadData" v-model="pagination.per_page" class="form-control right pagenum-list" debounce="300">
+          <option>5</option>
+          <option>10</option>
+          <option>20</option>
+          <option>50</option>
+          <option>100</option>
+        </select>
+        
+      </div>                 
+    </div>
+    <div class="row">
+      <m-list :movies='movies'></m-list>
+    </div>        
+    <div class="row">
+      <div class="col-md-12"><v-pagination :pagination="pagination" :offset="3"></v-pagination></div>
+    </div>    
     
   </div>
 </template>
 
 <script>
+import vPagination from 'vue-bootstrap-pagination'
 import mList from './components/movieList'
 
 export default {
+  destroyed () {
+    console.log('module movApp: destroyed')
+  },
+  beforeDestroy () {
+    console.log('module movApp: beforeDestroy')
+  },
+  created () {
+    console.log('module movApp: created')
+    if (this.movies && this.movies.length === 0) {
+      this.loadData()
+    }
+  },
+  init () {
+    console.log('module movApp: init')
+  },
   data () {
     return {
+      movies: [],
+      // resource_url: '/papi/movies'
+      pagination: {
+        last_page: 0,
+        per_page: 10,
+        from: 1,
+        to: 1,
+        current_page: 1
+      }
+    }
+  },
+  events: {
+    evtPageChanged: function (page) {
+      // if (this.pagination.current_page !== page) {
+      //   this.pagination.current_page = page
+      //   this.loadData()
+      // }
+      // this.pagination.current_page = page
+      this.loadData()
+    }
+  },
+  methods: {
+    // add methods here
+    loadData: function () {
+      this.$http.get('/papi/movies/' + this.pagination.current_page + '/' + this.pagination.per_page).then(function (response) {
+        var rdata = JSON.parse(response.data)
+        this.$set('movies', rdata.movies)
+        this.$set('pagination', rdata.pagination)
+        // save it to the app cache
+        this.$dispatch('evtGetMoviesData', rdata.movies)
+      }, function (error) {
+        // handle error
+        console.log('get movies data failed!!!' + error)
+      })
     }
   },
   components: {
-    mList
+    mList,
+    vPagination
   }
 }
 </script>
@@ -44,9 +115,17 @@ body {
   color: #42b983;
   text-decoration: none;
 }
-/*
-.logo {
-  width: 100px;
-  height: 100px
-}*/
+.right {
+    margin: 20px 0;
+    float: right;
+    /*width: 300px;
+    border: 3px solid #73AD21;
+    padding: 10px;
+    vertical-align: middle;*/
+    /*align-items:center;
+    align-content:center;*/
+}
+.pagenum-list {
+  width: 75px;
+}
 </style>
